5ee4fedfdcf8af8b55e56dc35a312036daca2deb
Saurabh <saurabhverma.iitd@gmail.com>
2021-03-11 18:50:27 +0530

    changes in accessing and printing function added

--- Assignment 3/Interpreter.cpp
+++ Assignment 3/Interpreter.cpp
@@ -9,10 +9,13 @@ class MIPS {
     int hi,low;
     unordered_map<string,string> asciiz;
     unordered_map<string,int> word;
+    unordered_map<string,int> regName={{"$zero",0},{"$at",1},{"$v0",2},{"$v1",3},{"$a0",4},{"$a1",5},{"$a2",6},{"$a3",7},{"$t0",8},{"$t1",9},{"$t2",10},{"$t3",11},{"$t4",12},{"$t5",13},{"$t6",14},{"$t7",15},{"$s0",16},{"$s1",17},{"$s2",18},{"$s3",19},{"$s4",20},{"$s5",21},{"$s6",22},{"$s7",23},{"$t8",24},{"$t9",25},{"$k0",26},{"$k1",27},{"$gp",28},{"$sp",29},{"$fp",30},{"$ra",31}};   
+    string arr[32]={"$zero","$at","$v0","$v1","$a0","$a1","$a2","$a3","$t0","$t1","$t2","$t3","$t4","$t5","$t6","$t7","$s0","$s1","$s2","$s3","$s4","$s5","$s6","$s7","$t8","$t9","$k0","$k1","$gp","$sp","$fp","$ra"};
     Stack S;
     public:
-    int getRegValue(int k){
-        return regs[k];
+    int getRegValue(string s){
+        int no =getRegNo(s);
+        return regs[no];
     }
     void stringIntoM(string name ,string value){
         asciiz[name]=value;
@@ -50,12 +53,13 @@ class MIPS {
             cerr<<msg<<endl;
         }
     }
-    void feedReg(int k,int value){
-        if (k==0){
+    void feedReg(string s,int value){
+        int no=getRegNo(s);
+        if (no==0){
             throw "cannot modify $zero register"; 
         }
         else {
-            regs[k]=value;
+            regs[no]=value;
         }
     }
     int getLow(){
@@ -64,9 +68,25 @@ class MIPS {
     int getHigh(){
         return hi;
     }
+    int getRegNo(string regN){
+        unordered_map<string,int>:: iterator it;
+        it=regName.find(regN);
+        if (it==regName.end()){
+            throw "Register name not correct";    
+        }
+        else {
+            return (it->second);
+        }
+    }
+    void printRegValue(string s){
+        cout<<s<<endl;
+        cout<<"[";
+        for(int i=0;i<31;i++){
+            cout<<arr[i]<<":"<<decToHex(regs[i])<<",";
+        }
+        cout<<arr[31]<<":"<<decToHex(regs[31])<<"]"<<endl;
+    }
 };
-
-
 
 
 
@@ -95,3 +115,22 @@ class Stack {
         }
     }
 };
+
+string decToHex(int dec){
+    string str="";
+    char c;
+    while (dec !=0){
+        int temp=0;
+        temp=dec%16;
+        if (temp<10){
+            c=temp+48;
+        }
+        else {
+            c=temp+55;
+        }
+        string s(1,c);
+        str=s+str;
+        dec= dec /16;
+    }
+    return str;
+}
